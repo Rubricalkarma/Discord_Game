239f44278c04a3a8592d2ae95f38f97d2a78ff7a
Rubricalkarma <syrusbomberger@yahoo.com>
2020-02-26 23:45:43 -0600

    Reaction stuff

--- commands/stats.js
+++ commands/stats.js
@@ -9,8 +9,14 @@ module.exports = {
                 message.reply(`You don't have an account yet!`);
             }else{
             const date = result.timeCreated;
-            message.channel.send(`Your account was created on ${date.getMonth()+1}-${date.getDate()}-${date.getFullYear()} `);
-            message.react('682130663553499146');
+            message.channel.send(`Your account was created on ${date.getMonth()+1}-${date.getDate()}-${date.getFullYear()} `)
+                .then(function(msg){
+                 msg.react('682130663553499146');
+                 const filter = (reaction) => reaction.emoji.name = 'thumbsup';
+                 const collector = msg.createReactionCollector(filter, {time: 15000})
+                 collector.on('collect', r => console.log(r.emoji.name));
+                 collector.on('end', collected => console.log(collected));
+            }).catch(console.error);
             //message.channel.send("<:WoW:682130663553499146>");
             }
         }).catch(function(error){
